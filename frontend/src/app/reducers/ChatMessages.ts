3a900deaf0cdcc864a84c0f282283dac6c3f507f
Ben Clare <ben.clare2@gmail.com>
2021-05-29 19:48:42 +1000

    feat: learning new redux pattern

---
+++ frontend/src/app/reducers/ChatMessages.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface Message {
+    authorUserId: string
+    content: string
+    sentTimeUtc: number
+}
+
+export interface ChatMessagesState {
+    messages: Message[]
+}
+
+const initialState: ChatMessagesState = {
+    messages: []
+}
+
+export const chatMessages = createSlice({
+    name: 'ChatMessages',
+    initialState,
+    reducers: {
+        addMessage: (state, action: { type: string, payload: Message }) => {
+            state.messages.push(action.payload)
+        }
+    }
+})
